86e2628e48cfb75c0678f9e90929e84dc1f3113a
Zach Robert <zrobert@mojotech.com>
2022-05-20 16:19:11 -0400

    [format] CI for credo and mix format

--- test/release_notes_bot_web/views/error_view_test.exs
+++ test/release_notes_bot_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule ReleaseNotesBotWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ReleaseNotesBotWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ReleaseNotesBotWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
