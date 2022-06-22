912e0517728a1cf4ee589f295dc2e49b0175f2bd
Zach Robert <zrobert@mojotech.com>
2022-06-22 10:41:55 -0400

    [feat] slack bot captures channels and users to persist to db

--- lib/release_notes_bot/channels.ex
+++ lib/release_notes_bot/channels.ex
@@ -20,4 +20,29 @@ defmodule ReleaseNotesBot.Channels do
   def get(param) do
     Repo.get_by(Channel, param)
   end
+
+  @spec register(binary, binary) :: nil
+  def register(slack_name, slack_id) do
+    case get(slack_id: slack_id) do
+      nil ->
+        if not parse_dm(slack_name) and not parse_group_dm(slack_name) do
+          create(%{"slack_id" => slack_id, "name" => slack_name})
+        end
+
+        nil
+
+      _ ->
+        nil
+    end
+  end
+
+  @spec parse_dm(binary) :: boolean
+  def parse_dm("directmessage"), do: true
+
+  def parse_dm(_channel), do: false
+
+  @spec parse_group_dm(binary) :: boolean
+  def parse_group_dm(channel_name) do
+    String.starts_with?(channel_name, "mpdm")
+  end
 end
