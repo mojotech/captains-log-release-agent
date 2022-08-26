a9896f90ecab84bd927831d795063f33e457fc29
wogara <weogara@gmail.com>
2022-08-26 15:08:30 -0400

    make post request to obtain access/refresh tokens

--- lib/release_notes_bot_web/controllers/oauth_controller.ex
+++ lib/release_notes_bot_web/controllers/oauth_controller.ex
@@ -6,6 +6,25 @@ defmodule ReleaseNotesBotWeb.OAuthController do
   use ReleaseNotesBotWeb, :controller
 
   def get(conn, params) do
+    slack_id = params["state"]
+
+    headers = [
+      {"Content-Type", "application/json"}
+    ]
+
+    body = %{
+      grant_type: "authorization_code",
+      client_id: Application.get_env(:release_notes_bot, :client_id),
+      client_secret: Application.get_env(:release_notes_bot, :client_secret),
+      code: params["code"],
+      redirect_uri: Application.get_env(:release_notes_bot, :oauth_redirect_uri)
+    }
+
+    {_status, json_body} = JSON.encode(body)
+
+    tokens_response =
+      HTTPoison.post!("https://auth.atlassian.com/oauth/token", json_body, headers)
+
     conn |> Plug.Conn.send_resp(200, "OK")
   end
 end
