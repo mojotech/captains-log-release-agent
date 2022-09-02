f4d322bba669cf332e3ab868e9a380b0c6721a90
Zach Robert <zrobert@mojotech.com>
2022-09-02 15:46:26 -0400

    [feat] add webhook event schema

---
+++ lib/release_notes_bot/schema/webhook_event.ex
@@ -0,0 +1,23 @@
+defmodule ReleaseNotesBot.Schema.WebhookEvent do
+  @moduledoc """
+  This module is used to model webhook events.
+  A Repository can have 0 or many webhook events.
+  webhook events must be persisted to a Persistence Provider.
+  Think of webhook events as the automated version of notes.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "webhook_events" do
+    field(:raw_payload, :string)
+    belongs_to(:repository, ReleaseNotesBot.Schema.Repository)
+
+    timestamps()
+  end
+
+  def changeset(webhook_event, params) do
+    webhook_event
+    |> cast(params, [:repository_id, :raw_payload])
+    |> validate_required([:repository_id, :raw_payload])
+  end
+end
---
+++ lib/release_notes_bot/webhook_events.ex
@@ -0,0 +1,27 @@
+defmodule ReleaseNotesBot.WebhookEvents do
+  @moduledoc """
+  Repo functions for Webhook Events
+  """
+  alias ReleaseNotesBot.Repo
+  alias ReleaseNotesBot.Schema.WebhookEvent
+
+  def create(params) do
+    %WebhookEvent{}
+    |> WebhookEvent.changeset(params)
+    |> Repo.insert()
+  end
+
+  def get_all do
+    Repo.all(WebhookEvent)
+  end
+
+  def get(param) do
+    Repo.get_by(WebhookEvent, param)
+  end
+
+  def create_async(repo_id, payload) do
+    Task.async(fn ->
+      create(%{:raw_payload => payload, :repository_id => repo_id})
+    end)
+  end
+end
