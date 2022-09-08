f1a2038ef2301f0c06eaaae687bbfba8d775f49e
Zach Robert <zrobert@mojotech.com>
2022-09-08 13:21:52 -0400

    [refac] all Slack message posting to channels.ex

--- lib/release_notes_bot_web/controllers/slack_interaction_controller.ex
+++ lib/release_notes_bot_web/controllers/slack_interaction_controller.ex
@@ -4,34 +4,32 @@ defmodule ReleaseNotesBotWeb.SlackInteractionController do
   / commands from Slack.
   """
   use ReleaseNotesBotWeb, :controller
-  alias ReleaseNotesBot.Projects
+  alias ReleaseNotesBot.{Projects, Channels}
 
   def index(conn, params) do
     %{"view" => view, "user" => user} = Projects.parse_params(params)
 
     case Projects.parse_response(view) do
       %{client: client_name, channel: channel} ->
-        Slack.Web.Chat.post_message(
+        Channels.post_message(
           channel,
           "#{user["name"]} has configured this channel to accept messages and updates for projects under: #{client_name}"
         )
 
       %{client: client_name, project: project_name} ->
-        Slack.Web.Chat.post_message(
+        Channels.post_message(
           Application.get_env(:release_notes_bot, :slack_channel),
           "#{user["name"]} has created a new project for #{client_name} titled: '#{project_name}'"
         )
 
       %{details: details, client: client} when client.channels != nil ->
-        Enum.each(client.channels, fn c ->
-          Slack.Web.Chat.post_message(
-            c.slack_id,
-            "<!here>\n#{user["name"]} has posted a Release Note to '#{details.project}' titled: '#{details.title}'.\nDetails:\n#{details.message}\n\nPersistence Status: #{details.persistence_status}"
-          )
-        end)
+        Channels.post_message_all_client_channels(
+          client,
+          "<!here>\n#{user["name"]} has posted a Release Note to '#{details.project}' titled: '#{details.title}'.\nDetails:\n#{details.message}\n\nPersistence Status: #{details.persistence_status}"
+        )
 
       %{client: client_name} ->
-        Slack.Web.Chat.post_message(
+        Channels.post_message(
           Application.get_env(:release_notes_bot, :slack_channel),
           "#{user["name"]} has created new client: #{client_name}"
         )
